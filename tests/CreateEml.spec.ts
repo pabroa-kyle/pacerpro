e5fd544cf1c2418873ebee041b9e4602246ac5e8
rob132527 <romeo.briones@uberall.com>
2025-02-18 08:49:05 +0800

    Initial commit

--- tests/CreateEml.spec.ts
+++ tests/CreateEml.spec.ts
@@ -18,19 +18,18 @@ test('should create a new electronic mail', async ({ page }) => {
   await page.getByRole('link', { name: 'New Electronic Mail' }).click();
 
   // Fill the form for the new electronic mail
-  await newMailPage.fillFromField('efile@nycourts.gov');
-  await newMailPage.fillToField('qa@cloudmailin.codevelopr.com');
+  await newMailPage.fillFromField('eservice@myflcourtaccess.com');
   await newMailPage.fillToField('engineering.qa@pacerpro.com'); // Correct the email
   await newMailPage.checkApiCheckbox();
   await newMailPage.selectInboundUrl();
   //await newMailPage.optionInboundUrl();
-  await newMailPage.uploadEmlFile('/Users/macbookpro/Documents/31918360.eml');
+  await newMailPage.uploadEmlFile('/Users/macbookpro/Documents/31951049.eml');
 
   // Submit the form
   await newMailPage.submitForm();
 
   // Optionally, verify the result (you can adjust based on the expected result after submission)
-  await expect(page).toHaveURL('/admin/electronic_mails');
+  await expect(page).toHaveURL(/\/admin\/electronic_mails/);
 
   await page.pause()
 });
